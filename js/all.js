ff2cc4c55276524d28cad77aed79305b5ff69186
maruelle-gulfan <77771444+maruelle-gulfan@users.noreply.github.com>
2022-05-20 13:15:39 +0800

    Add files via upload

--- js/all.js
+++ js/all.js
@@ -42,3 +42,100 @@ function deleting(textIndex, text) {
     }
   }, sPerChar * 1000);
 }
+
+window.onload = function () {
+  window.addEventListener("scroll", function (e) {
+    if (window.pageYOffset > 30) {
+      document.querySelector("header").classList.add("change-color");
+    } else {
+      document.querySelector("header").classList.remove("change-color");
+    }
+  });
+
+  document.querySelector(".html").classList.add("skill-active");
+
+  document.getElementById("hexagon").addEventListener("click", function () {
+    if (this.classList.contains("hex-html")) {
+      document.querySelector(".css").classList.remove("skill-active");
+      document.querySelector(".react").classList.remove("skill-active");
+      document.querySelector(".js").classList.remove("skill-active");
+      document.querySelector(".node").classList.remove("skill-active");
+      document.querySelector(".wp").classList.remove("skill-active");
+      document.querySelector(".sass").classList.remove("skill-active");
+
+      document.querySelector(".html").classList.add("skill-active");
+    }
+  });
+  document.getElementById("hexagon2").addEventListener("click", function () {
+    if (this.classList.contains("hex-css")) {
+      document.querySelector(".html").classList.remove("skill-active");
+      document.querySelector(".react").classList.remove("skill-active");
+      document.querySelector(".js").classList.remove("skill-active");
+      document.querySelector(".node").classList.remove("skill-active");
+      document.querySelector(".wp").classList.remove("skill-active");
+      document.querySelector(".sass").classList.remove("skill-active");
+
+      document.querySelector(".css").classList.add("skill-active");
+    }
+  });
+  document.getElementById("hexagon3").addEventListener("click", function () {
+    if (this.classList.contains("hex-react")) {
+      document.querySelector(".css").classList.remove("skill-active");
+      document.querySelector(".html").classList.remove("skill-active");
+      document.querySelector(".js").classList.remove("skill-active");
+      document.querySelector(".node").classList.remove("skill-active");
+      document.querySelector(".wp").classList.remove("skill-active");
+      document.querySelector(".sass").classList.remove("skill-active");
+
+      document.querySelector(".react").classList.add("skill-active");
+    }
+  });
+  document.getElementById("hexagon4").addEventListener("click", function () {
+    if (this.classList.contains("hex-js")) {
+      document.querySelector(".css").classList.remove("skill-active");
+      document.querySelector(".react").classList.remove("skill-active");
+      document.querySelector(".html").classList.remove("skill-active");
+      document.querySelector(".node").classList.remove("skill-active");
+      document.querySelector(".wp").classList.remove("skill-active");
+      document.querySelector(".sass").classList.remove("skill-active");
+
+      document.querySelector(".js").classList.add("skill-active");
+    }
+  });
+  document.getElementById("hexagon5").addEventListener("click", function () {
+    if (this.classList.contains("hex-node")) {
+      document.querySelector(".css").classList.remove("skill-active");
+      document.querySelector(".react").classList.remove("skill-active");
+      document.querySelector(".js").classList.remove("skill-active");
+      document.querySelector(".html").classList.remove("skill-active");
+      document.querySelector(".wp").classList.remove("skill-active");
+      document.querySelector(".sass").classList.remove("skill-active");
+
+      document.querySelector(".node").classList.add("skill-active");
+    }
+  });
+  document.getElementById("hexagon6").addEventListener("click", function () {
+    if (this.classList.contains("hex-wp")) {
+      document.querySelector(".css").classList.remove("skill-active");
+      document.querySelector(".react").classList.remove("skill-active");
+      document.querySelector(".js").classList.remove("skill-active");
+      document.querySelector(".node").classList.remove("skill-active");
+      document.querySelector(".html").classList.remove("skill-active");
+      document.querySelector(".sass").classList.remove("skill-active");
+
+      document.querySelector(".wp").classList.add("skill-active");
+    }
+  });
+  document.getElementById("hexagon7").addEventListener("click", function () {
+    if (this.classList.contains("hex-sass")) {
+      document.querySelector(".css").classList.remove("skill-active");
+      document.querySelector(".react").classList.remove("skill-active");
+      document.querySelector(".js").classList.remove("skill-active");
+      document.querySelector(".node").classList.remove("skill-active");
+      document.querySelector(".wp").classList.remove("skill-active");
+      document.querySelector(".html").classList.remove("skill-active");
+
+      document.querySelector(".sass").classList.add("skill-active");
+    }
+  });
+};
